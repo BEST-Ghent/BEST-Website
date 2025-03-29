5402cb4860235c214b13137416ba588d39dddc51
HannesCoulembier <hannes.coulembier@gmail.com>
2025-03-29 21:31:51 +0100

    Fixed FOUC (hopefully)

--- static/scripts/templates.js
+++ static/scripts/templates.js
@@ -1,3 +1,8 @@
+// Removes all fouc-barrier classes in the document
+function removeFOUCbarriers() {
+    let instances = document.getElementsByClassName("fouc-barrier");
+	[...instances].forEach(element => { element.classList.remove("fouc-barrier"); });
+}
 
 // Creates a fragment from the given htmlStr
 function createFragment(htmlStr) {
@@ -20,38 +25,50 @@ function applyTemplate(indicator, fragmentTxt) {
 
 }
 
+let loadedStyleSheets = 0;
+function loadedStylesheetCallback() {
+	loadedStyleSheets++;
+	
+	let instances = document.getElementsByClassName("dynamic-stylesheet")
+	let totalStyleSheets = [...instances].length
+	if (loadedStyleSheets==totalStyleSheets) {
+		removeFOUCbarriers();
+	}
+}
+
 // This is where all the templates are defined
 function applyTemplates() {
+
 
     applyTemplate("templates-head",`
 	<meta name="viewport" content="width=device-width">
 	<meta charset="UTF-8">
 
 	<!-- CSS -->
-	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.7.1/jquery.fullPage.css">
-	<link rel="stylesheet" type="text/css" href="/static/stylesheets/style-v1.css">
+	<link rel="stylesheet" type="text/css" onload="loadedStylesheetCallback();" class="dynamic-stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" onload="loadedStylesheetCallback();" class="dynamic-stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.7.1/jquery.fullPage.css">
+	<link rel="stylesheet" type="text/css" onload="loadedStylesheetCallback();" class="dynamic-stylesheet" href="/static/stylesheets/style-v1.css">
 
 	<!-- FONTS -->
-	<link href='https://fonts.googleapis.com/css?family=Open+Sans:100,300,400,700' rel='stylesheet' type='text/css'>
-	<link href='https://fonts.googleapis.com/css?family=Lato:400,200,700,300' rel='stylesheet' type='text/css'>
+	<link rel='stylesheet' type='text/css' onload="loadedStylesheetCallback();" class="dynamic-stylesheet" href='https://fonts.googleapis.com/css?family=Open+Sans:100,300,400,700'>
+	<link rel='stylesheet' type='text/css' onload="loadedStylesheetCallback();" class="dynamic-stylesheet" href='https://fonts.googleapis.com/css?family=Lato:400,200,700,300'>
     <!-- font awesome icons -->
-	<link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+	<link rel='stylesheet' type='text/css' onload="loadedStylesheetCallback();" class="dynamic-stylesheet" href="https://use.fontawesome.com/releases/v5.0.6/css/all.css">
 	
 	<!-- ICONS -->
-	<link rel="apple-touch-icon" sizes="57x57" href="/static/media/ico/apple-touch-icon-57x57.png">
-	<link rel="apple-touch-icon" sizes="60x60" href="/static/media/ico/apple-touch-icon-60x60.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="/static/media/ico/apple-touch-icon-72x72.png">
-	<link rel="apple-touch-icon" sizes="76x76" href="/static/media/ico/apple-touch-icon-76x76.png">
-	<link rel="apple-touch-icon" sizes="114x114" href="/static/media/ico/apple-touch-icon-114x114.png">
-	<link rel="apple-touch-icon" sizes="120x120" href="/static/media/ico/apple-touch-icon-120x120.png">
-	<link rel="apple-touch-icon" sizes="144x144" href="/static/media/ico/apple-touch-icon-144x144.png">
-	<link rel="apple-touch-icon" sizes="152x152" href="/static/media/ico/apple-touch-icon-152x152.png">
-	<link rel="apple-touch-icon" sizes="180x180" href="/static/media/ico/apple-touch-icon-180x180.png">
-	<link rel="icon" type="image/png" href="/static/media/ico/favicon-32x32.png" sizes="32x32">
-	<link rel="icon" type="image/png" href="/static/media/ico/android-chrome-192x192.png" sizes="192x192">
-	<link rel="icon" type="image/png" href="/static/media/ico/favicon-96x96.png" sizes="96x96">
-	<link rel="icon" type="image/png" href="/static/media/ico/favicon-16x16.png" sizes="16x16">
+	<link rel="apple-touch-icon" type="image/png" sizes="57x57"   href="/static/media/ico/apple-touch-icon-57x57.png">
+	<link rel="apple-touch-icon" type="image/png" sizes="60x60"   href="/static/media/ico/apple-touch-icon-60x60.png">
+	<link rel="apple-touch-icon" type="image/png" sizes="72x72"   href="/static/media/ico/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" type="image/png" sizes="76x76"   href="/static/media/ico/apple-touch-icon-76x76.png">
+	<link rel="apple-touch-icon" type="image/png" sizes="114x114" href="/static/media/ico/apple-touch-icon-114x114.png">
+	<link rel="apple-touch-icon" type="image/png" sizes="120x120" href="/static/media/ico/apple-touch-icon-120x120.png">
+	<link rel="apple-touch-icon" type="image/png" sizes="144x144" href="/static/media/ico/apple-touch-icon-144x144.png">
+	<link rel="apple-touch-icon" type="image/png" sizes="152x152" href="/static/media/ico/apple-touch-icon-152x152.png">
+	<link rel="apple-touch-icon" type="image/png" sizes="180x180" href="/static/media/ico/apple-touch-icon-180x180.png">
+	<link rel="icon"             type="image/png" sizes="16x16"   href="/static/media/ico/favicon-16x16.png">
+	<link rel="icon"             type="image/png" sizes="32x32"   href="/static/media/ico/favicon-32x32.png">
+	<link rel="icon"             type="image/png" sizes="96x96"   href="/static/media/ico/favicon-96x96.png">
+	<link rel="icon"             type="image/png" sizes="192x192" href="/static/media/ico/android-chrome-192x192.png">
 	<link rel="manifest" href="/static/media/ico/manifest.json">
 	<link rel="mask-icon" href="/static/media/ico/safari-pinned-tab.svg" color="#0572ba">
 	<link rel="shortcut icon" href="/static/media/ico/favicon.ico">
@@ -74,11 +91,4 @@ function applyTemplates() {
 `);
 }
 
-// Removes all fouc-barrier classes in the document
-function removeFOUCbarriers() {
-    let instances = document.getElementsByClassName("fouc-barrier");
-	[...instances].forEach(element => { element.classList.remove("fouc-barrier"); });
-}
-
 applyTemplates();
-removeFOUCbarriers();
